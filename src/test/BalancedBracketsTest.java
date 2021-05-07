4b377e7bf76fe9f653bf3cb688365a48931a6e61
SkOoLyD <79675305+SkOoLyD@users.noreply.github.com>
2021-05-07 02:32:21 -0500

    Junit Testing

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,6 +1,8 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
+
 
 import static org.junit.Assert.*;
 
@@ -10,6 +12,53 @@ public class BalancedBracketsTest {
     @Test
     public void emptyTest() {
         assertEquals(true, true);
+    }
+
+    //Test for Improper brackets
+    @Test
+    public void incorrectBracketFail() {
+        assertEquals(false, BalancedBrackets.hasBalancedBrackets("[TEST["));
+
+    }
+    @Test
+    public void oneOpeningBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+
+    }
+    @Test
+    public void oneClosingBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+
+    }
+    @Test
+    public void incorrectOrderOfBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+
+    }
+    @Test
+    public void testTenNestedPairs() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[[[[[[[[]]]]]]]]]]"));
+
+    }
+    @Test
+    public void TenUnorderedPairs() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[][[[[]][]]]]"));
+
+    }
+    @Test
+    public void testForEmptyString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+
+    }
+    @Test
+    public void testForEmptyBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+
+    }
+    @Test
+    public void testForBouncingBackPastZero() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[]]]["));
+
     }
 
 
